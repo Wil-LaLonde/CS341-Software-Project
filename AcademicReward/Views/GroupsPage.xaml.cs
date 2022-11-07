ae5002c93838ce15fdca8d8d351af2ab3b43bb82
Maximilian <max@mpatterson.xyz>
2022-11-06 19:23:27 -0600

    Groups view (not working, weird crash error)

--- AcademicReward/Views/GroupsPage.xaml.cs
+++ AcademicReward/Views/GroupsPage.xaml.cs
@@ -1,9 +1,32 @@
+using System.Collections.ObjectModel;
+
 namespace AcademicReward.Views;
 
 public partial class GroupsPage : ContentPage
 {
-	public GroupsPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Group> Groups = new ObservableCollection<Group>();
+    public GroupsPage()
+    {
+        InitializeComponent();
+        GroupsLV.ItemsSource = Groups;
+        TestData();
+    }
+
+    public void TestData()
+    {
+        base.OnAppearing();
+
+        // Add some test data
+        Groups.Add(new Group { Name = "Group 1" });
+        Groups.Add(new Group { Name = "Group 2" });
+        Groups.Add(new Group { Name = "Group 3" });
+        Groups.Add(new Group { Name = "Group 4" });
+        Groups.Add(new Group { Name = "Group 5" });
+    }
+
+    // Private class for HistoryItem
+    public class Group
+    {
+        public string Name { get; set; }
+    }
 }
