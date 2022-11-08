30de80b6ec6073562eab64ddf1d722d678e43dc8
Sean Stille <seanstille0627@gmail.com>
2022-11-08 13:36:20 -0600

    Shop page created and merged with other files

--- AcademicReward/Views/ShopPage.xaml.cs
+++ AcademicReward/Views/ShopPage.xaml.cs
@@ -1,7 +1,24 @@
+using CommunityToolkit.Maui.Views;
+
 namespace AcademicReward.Views;
 
 public partial class ShopPage : ContentPage {
 	public ShopPage() {
 		InitializeComponent();
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		this.ShowPopup(new AddShopItemPage());
+	}
+
+    private void Ticket_Clicked(object sender, EventArgs e)
+    {
+       this.ShowPopup(new ViewShopItemPage());
+    }
+
+    private void Shirt_Clicked(object sender, EventArgs e)
+    {
+        
+    }
 }
