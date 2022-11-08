dc4faafe73680bd0a084d8e494b0e161c1cc362d
Maximilian <max@mpatterson.xyz>
2022-11-07 18:09:37 -0600

    Group page with test data

--- AcademicReward/Views/GroupPage.xaml.cs
+++ AcademicReward/Views/GroupPage.xaml.cs
@@ -1,9 +1,30 @@
+using System.Collections.ObjectModel;
+
 namespace AcademicReward.Views;
 
 public partial class GroupPage : ContentPage
 {
-	public GroupPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Member> Members = new ObservableCollection<Member>();
+    public GroupPage()
+    {
+        InitializeComponent();
+        MembersLV.ItemsSource = Members;
+        TestData();
+    }
+
+    public void TestData()
+    {
+        base.OnAppearing();
+
+        // Add some test data
+        Members.Add(new Member { Name = "Billy" });
+        Members.Add(new Member { Name = "Tom" });
+        Members.Add(new Member { Name = "Bob" });        
+    }
+
+    // Private class for HistoryItem
+    public class Member
+    {
+        public string Name { get; set; }
+    }
 }
