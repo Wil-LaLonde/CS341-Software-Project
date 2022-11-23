887214c671efb9f371810ea45ab74f8eeb0311a3
Wil -LaLonde <wil.lalonde@gmail.com>
2022-11-23 15:40:16 -0600

    WL - Member Notification Page

---
+++ AcademicReward/Logic/NotificationLogic.cs
@@ -0,0 +1,34 @@
+﻿using AcademicReward.Database;
+using AcademicReward.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicReward.Logic {
+    public class NotificationLogic : ILogic {
+        IDatabase notificationDB;
+
+        /// <summary>
+        /// NotificationLogic constructor
+        /// </summary>
+        public NotificationLogic() { notificationDB = new NotificationDatabase(); }
+
+        public LogicErrorType AddItem(object notification) {
+            return LogicErrorType.NoError;
+        }
+
+        public LogicErrorType UpdateItem(object notification) {
+            return LogicErrorType.NoError;
+        }
+
+        public LogicErrorType DeleteItem(object notification) {
+            return LogicErrorType.NoError;
+        }
+
+        public LogicErrorType LookupItem(object notification) {
+            return LogicErrorType.NoError;
+        }
+    }
+}
--- AcademicReward/Views/NotificationPage.xaml.cs
+++ AcademicReward/Views/NotificationPage.xaml.cs
@@ -1,7 +1,12 @@
+using AcademicReward.Logic;
+
 namespace AcademicReward.Views;
 
 public partial class NotificationPage : ContentPage {
+    ILogic notificationLogic;
+
 	public NotificationPage() {
 		InitializeComponent();
-	}
+        notificationLogic = new NotificationLogic();
+    }
 }
