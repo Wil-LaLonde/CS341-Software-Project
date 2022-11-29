975390fa2e19300b6bd75c95c5cab98f06d4d19a
Wil -LaLonde <wil.lalonde@gmail.com>
2022-11-29 09:01:50 -0600

    WL - Add TaskDatabase Class

---
+++ AcademicReward/Database/TaskDatabase.cs
@@ -0,0 +1,30 @@
+﻿using AcademicReward.Resources;
+
+namespace AcademicReward.Database {
+    public class TaskDatabase : AcademicRewardsDatabase, IDatabase {
+
+        public TaskDatabase() { }
+
+        public DatabaseErrorType AddItem(object task) {
+            return DatabaseErrorType.NoError;
+        }
+
+        //Currently not needed
+        public DatabaseErrorType UpdateItem(object task) {
+            return DatabaseErrorType.NoError;
+        }
+
+        //Currently not needed
+        public DatabaseErrorType DeleteItem(object task) {
+            return DatabaseErrorType.NoError;
+        }
+
+        public DatabaseErrorType LookupItem(object task) {
+            return DatabaseErrorType.NoError;
+        }
+
+        public DatabaseErrorType LookupFullItem(object task) {
+            return DatabaseErrorType.NoError;
+        }
+    }
+}
