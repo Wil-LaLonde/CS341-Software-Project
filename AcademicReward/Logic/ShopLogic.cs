cdcc22e6631f08d78d939b57d546334b1da229aa
Sean Stille <seanstille0627@gmail.com>
2022-11-30 05:22:38 -0600

    Implemented Admin Shop SQL features, still needs a little tweaking though

--- AcademicReward/Logic/ShopLogic.cs
+++ AcademicReward/Logic/ShopLogic.cs
@@ -1,11 +1,13 @@
 ﻿using AcademicReward.Database;
 using AcademicReward.Resources;
+using AcademicReward.ModelClass;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 namespace AcademicReward.Logic
 {
     /**
@@ -16,9 +18,11 @@ namespace AcademicReward.Logic
     public class ShopLogic : ILogic
     {
         IDatabase ShopData;
+        public ObservableCollection<ShopItem> ItemList;
         public ShopLogic()
         {
-            ShopData = new ShopItemDatabase();
+            ShopData = new ShopItemDatabase(this);
+            ItemList = new ObservableCollection<ShopItem>();
         }
         public LogicErrorType AddItem(object obj)
         {
@@ -35,25 +39,68 @@ namespace AcademicReward.Logic
                 //return LogicErrorType.InvalidLevel;
             }
 
-            toBeAdded = new ModelClass.ShopItem(AddItemVals[0], AddItemVals[1], cost, level, null);
-
-            ShopData.AddItem(toBeAdded);
-
-            return LogicErrorType.NoError;
+            toBeAdded = new ShopItem(AddItemVals[0], AddItemVals[1], cost, level, null);
+            
+            
+            ItemList.Add(toBeAdded);
+            if (ShopData.AddItem(toBeAdded) == DatabaseErrorType.NoError)
+            {
+                return LogicErrorType.NoError;
+            }
+            else
+            {
+                return LogicErrorType.LookupAllTasksDBError;
+            }
         }
 
         public LogicErrorType DeleteItem(object obj)
         {
-            throw new NotImplementedException();
+            ShopItem item = obj as ShopItem;
+            ShopItem toBeRemoved = null;
+            foreach ( ShopItem i in ItemList)
+            {
+                if (i.Id == item.Id)
+                {
+                    toBeRemoved = i;
+                }
+            }
+            ItemList.Remove(toBeRemoved);
+            ShopData.DeleteItem(toBeRemoved);
+            return LogicErrorType.NoError;
         }
 
         public LogicErrorType LookupItem(object obj)
         {
-            throw new NotImplementedException();
+            ShopData.LookupFullItem(null);
+            return LogicErrorType.NoError;
         }
 
         public LogicErrorType UpdateItem(object obj)
         {
+            ModelClass.ShopItem toBeAdded;
+            String[] AddItemVals = (String[])obj;
+            int cost = -1;
+            int level = -1;
+            if (!int.TryParse(AddItemVals[2], out cost)) //Testing cost, if it doesn't work, throw exception
+            {
+                //return LogicErrorType.InvalidCost;
+            }
+            if (!int.TryParse(AddItemVals[3], out level)) // Testing level
+            {
+                //return LogicErrorType.InvalidLevel;
+            }
+
+            toBeAdded = new ShopItem(int.Parse(AddItemVals[5]), AddItemVals[0], AddItemVals[1], cost, level, null);
+
+            
+            if (ShopData.UpdateItem(toBeAdded) == DatabaseErrorType.NoError)
+            {
+                return LogicErrorType.NoError;
+            }
+            else
+            {
+                return LogicErrorType.LookupAllTasksDBError;
+            }
             throw new NotImplementedException();
         }
     }
--- AcademicReward/Views/ShopPage.xaml.cs
+++ AcademicReward/Views/ShopPage.xaml.cs
@@ -1,5 +1,7 @@
 using AcademicReward.Logic;
 using CommunityToolkit.Maui.Views;
+using AcademicReward.ModelClass;
+using System.Collections.ObjectModel;
 
 namespace AcademicReward.Views;
 
@@ -10,21 +12,41 @@ namespace AcademicReward.Views;
 /// </summary>
 public partial class ShopPage : ContentPage {
 
-    private ILogic ShopLogic;
+    private ShopLogic ShopLogic;
 	public ShopPage() {
 		InitializeComponent();
         ShopLogic = new ShopLogic();
+        getShopItems();
 	}
 
 	private void Button_Clicked(object sender, EventArgs e) {
 		this.ShowPopup(new AddShopItemPage(ShopLogic));
+        getShopItems();
 	}
 
-    private void Ticket_Clicked(object sender, EventArgs e) {
-       this.ShowPopup(new ViewShopItemPage());
+    private void SelectedItem(object sender, SelectedItemChangedEventArgs e)
+    {
+        ShopItem selectedItem = e.SelectedItem as ShopItem;
+        if (selectedItem != null)
+        {
+            //ShopItem notificationPopup = new Shopitem();
+            //this.ShowPopup(notificationPopup);
+            ShopItem selected = e.SelectedItem as ShopItem;
+            this.ShowPopup(new ViewShopItemPage(selected, ShopLogic, this));
+            getShopItems();
+        }
     }
 
-    private void Shirt_Clicked(object sender, EventArgs e) {
+    public void openEditPage(ShopItem toBeEditted)
+    {
+        this.ShowPopup(new EditShopItemPage(ShopLogic, toBeEditted));
         
+    }
+
+    public void getShopItems()
+    {
+        ShopLogic.LookupItem(null);
+       // ShopLogic.ItemList.Add(new ShopItem("TestTitle", "TestDesc",0,0,null));
+        ShopItemList.ItemsSource = ShopLogic.ItemList;
     }
 }
