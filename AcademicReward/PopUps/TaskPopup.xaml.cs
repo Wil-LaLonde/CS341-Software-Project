5c8c370a5e15439a3eea40b0249e861c202f54bc
Xee Lo <114117389+xeelo2000@users.noreply.github.com>
2022-12-06 13:03:48 -0600

    added a new comment

--- AcademicReward/PopUps/TaskPopup.xaml.cs
+++ AcademicReward/PopUps/TaskPopup.xaml.cs
@@ -1,4 +1,7 @@
+using AcademicReward.Database;
+using AcademicReward.ModelClass;
 using CommunityToolkit.Maui.Views;
+using static Android.Provider.ContactsContract;
 
 namespace AcademicReward.PopUps;
 
@@ -10,10 +13,13 @@ namespace AcademicReward.PopUps;
 public partial class TaskPopUp : Popup {
 
     public ModelClass.Task SelectedTask { get; }
+    bool isAdmin;
+    IDatabase history;
     public TaskPopUp() {
 		InitializeComponent();
-		
-	}
+        
+
+    }
 
 	public TaskPopUp(ModelClass.Task selectedTask) {
         InitializeComponent();
@@ -22,7 +28,10 @@ public partial class TaskPopUp : Popup {
         description.Text = selectedTask.Description;
         points.Text = selectedTask.Points.ToString();
         group.Text = selectedTask.GroupID.ToString();
-	}
+        isAdmin = MauiProgram.Profile.IsAdmin;
+        history = new HistoryDatabase();
+
+    }
 
     /// <summary>
     /// Method that closes the popup
@@ -32,11 +41,68 @@ public partial class TaskPopUp : Popup {
     private void BackButtonClicked(object sender, EventArgs e) => Close();
 
     /// <summary>
-    /// Method that submits the task for review 
+    /// Method that submits the task for review
+    /// send notification (maybe) 
+    /// ProfileTask == table that shows up for individual task 
+    /// ProfileNotification == table that shows up nofication for individual 
+    /// need to make a sql command for Task == individual task from notification
+    /// if both bool is true then send it to history event
+    /// stop displaying for member
     /// </summary>
     /// <param name="sender">object sender</param>
     /// <param name="e">EventArgs e</param>
-    private void SubmitTask(object sender, EventArgs e) => Close();
+    private void SubmitTask(object sender, EventArgs e) {
+        if (isAdmin)
+        {
+            //call the task database to change the profileTask table
+            SelectedTask.IsChecked = true;
+            MauiProgram.Profile.RemoveTaskFromProfile(SelectedTask);
+            //need to get sql call for group to remove [SelectedTask.GroupID]
+            //MauiProgram.Profile.RemoveGroupFromProfile(Group group);
+        }
+        else
+        {
+            if (SelectedTask.IsChecked)//if the task is fully checked off 
+            {
+                //removes task from list 
+                MauiProgram.Profile.RemoveTaskFromProfile(SelectedTask);
+                //removes task from list
+
+                //send notification to memeber that they have been awarded 
+              //  Notification completedTaskNotification = new Notification("Congratulations, you have completed a task", $"Task: {SelectedTask.Title}", SelectedTask.GroupID);
+               // MauiProgram.Profile.AddNotificationToProfile(completedTaskNotification);
+                //send notification to memeber that they have been awarded 
+                //-NEED TO TALK ABOUT HOW NOTIFICATION WILL BE SENT TO GROUPS NOT TO YOURSELF
+
+                //add it to task to history
+                HistoryItem taskHistory = new HistoryItem(MauiProgram.Profile.ProfileID, SelectedTask.Title, $"{SelectedTask.Description}\nPoints: {SelectedTask.Points}\nGroupID: {SelectedTask.GroupID}");
+                history.AddItem(taskHistory);
+                //add it to task to history
+
+                //add points and exp --- exp should be discussed 
+                MauiProgram.Profile.AddXPToMember(50);
+                MauiProgram.Profile.AddPointsToMember(SelectedTask.Points);
+                //add points and exp --- exp should be discussed 
+
+                //need to get sql call for group to remove [SelectedTask.GroupID]
+
+                //MauiProgram.Profile.RemoveGroupFromProfile(Group group);
+            }
+            else //if the task is not fully checked then
+            {
+                //add the task to the admin tasklist 
+
+                //Notification completedTaskNotification = new Notification("A new Task as been added", $"Task: {SelectedTask.Title}", SelectedTask.GroupID);
+                //need to send this notification to admin
+
+                //need to figure out sql call 
+
+            }
+        }
+
+        Close();
+    }
+    
 
    
 }
