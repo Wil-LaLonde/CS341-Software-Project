7133d1986765aab95f74de3d232bfaf238fe0cb3
Wil-LaLonde <wil.lalonde@gmail.com>
2022-12-06 12:17:28 -0600

    WL - Remove Blank Line

--- AcademicReward/Views/LoginPage.xaml.cs
+++ AcademicReward/Views/LoginPage.xaml.cs
@@ -84,7 +84,6 @@ public partial class LoginPage : ContentPage {
 		LoginPopUp loginPopUp = new LoginPopUp();
 		Profile newProfile = await this.ShowPopupAsync(loginPopUp) as Profile;
 		if(newProfile != null) {
-            
 			await DisplayAlert(DataConstants.AddProfileSuccessTitle, DataConstants.AddProfileSuccessMessage, DataConstants.OK);
 		} 
     }
