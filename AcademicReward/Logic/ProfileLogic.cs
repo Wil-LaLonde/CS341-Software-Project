7ab171c67ec7b087642a3b42a78777790eb8aa36
xee <xee.lo.2000@gmail.com>
2022-12-11 23:14:06 -0600

    added ProfileDatabase and Logic
    This is updating the profiles Xp, Level, and Points

---
+++ AcademicReward/Logic/ProfileLogic.cs
@@ -0,0 +1,41 @@
+﻿using AcademicReward.Database;
+using AcademicReward.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicReward.Logic {
+    internal class ProfileLogic : ILogic{
+        IDatabase profileDB;
+        public ProfileLogic() {
+            profileDB = new ProfileDatabase();
+        }
+        public LogicErrorType AddItem(object obj) {
+            return LogicErrorType.NoError;
+        }
+        public LogicErrorType UpdateItem(object obj) {
+            LogicErrorType logicError;
+            Profile profileToUpdate = profile as Profile;
+            
+                
+                //Making database call to update salt and password (hash)
+                DatabaseErrorType dbError = profileDB.UpdateItem(profileToUpdate);
+                if (DatabaseErrorType.NoError == dbError) {
+                    logicError = LogicErrorType.NoError;
+                }
+                else {
+                    logicError = LogicErrorType.UpdatePasswordDBError;
+                }
+            
+            return logicError;
+        }
+        public LogicErrorType DeleteItem(object obj) {
+            return LogicErrorType.NoError;
+        }
+        public LogicErrorType LookupItem(object obj) {
+            return LogicErrorType.NoError;
+        }
+    }
+}
