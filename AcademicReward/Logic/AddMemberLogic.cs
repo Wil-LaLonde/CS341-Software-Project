c8151432f0ddea7a27e24c18920a4599b912762a
Wil-LaLonde <wil.lalonde@gmail.com>
2022-12-13 17:42:08 -0600

    WL - Add Member Error Messages

--- AcademicReward/Logic/AddMemberLogic.cs
+++ AcademicReward/Logic/AddMemberLogic.cs
@@ -26,29 +26,23 @@ namespace AcademicReward.Logic {
         /// <param name="obj">object[] obj</param>
         /// <returns>LogicErrorType logicError</returns>
         public LogicErrorType AddItemWithArgs(object[] obj) {
+            LogicErrorType logicError;
             // Convert object to array
             var arguments = obj as object[];
+            logicError = AddMemberCheck(arguments[0] as string);
+            if(LogicErrorType.NoError == logicError) {
+                // Find profile by username
+                var profile = GroupProfileRelationship.findByUsername(arguments[0] as string);
 
-            // Find profile by username
-            var profile = GroupProfileRelationship.findByUsername(arguments[0] as string);
-
-            // Set group from arguments
-            var group = arguments[1] as Group;
-
-            // Add relationship to database
-            var error = GroupProfileRelationship.addProfileToGroup(profile, group);
-            if (error != LogicErrorType.NoError) {
-                return error;
+                // Set group from arguments
+                var group = arguments[1] as Group;
+                logicError = GroupProfileRelationship.addProfileToGroup(profile, group);
+                if(LogicErrorType.NoError == logicError) {
+                    //Add history item
+                    historyDB.AddItem(new HistoryItem(MauiProgram.Profile.ProfileID, DataConstants.HistoryAddMemberToGroupGroupTitle, string.Format(DataConstants.HistoryAddMemberToGroupGroupDescription, profile.Username, group.GroupName)));
+                } 
             }
-
-
-            // Create history entry for a new member in group
-            try {
-                historyDB.AddItem(new HistoryItem(MauiProgram.Profile.ProfileID, DataConstants.HistoryAddMemberToGroupGroupTitle, string.Format(DataConstants.HistoryAddMemberToGroupGroupDescription, profile.Username, group.GroupName)));
-            } catch {
-                return LogicErrorType.HistoryAddError;
-            }
-            return LogicErrorType.NoError;
+            return logicError;
         }
 
         /// <summary>
@@ -75,6 +69,33 @@ namespace AcademicReward.Logic {
         //Currently not needed
         public LogicErrorType AddItem(object obj) {
             return LogicErrorType.NotImplemented;
+        }
+
+        /// <summary>
+        /// Helper method used to check the given member username
+        /// </summary>
+        /// <param name="username">string username</param>
+        /// <returns>LogicErrorType logicError</returns>
+        private LogicErrorType AddMemberCheck(string username) {
+            LogicErrorType logicError;
+            if(string.IsNullOrEmpty(username)) {
+                logicError = LogicErrorType.EmptyUsername;
+            } else if(CheckUsernameLength(username)) {
+                logicError = LogicErrorType.InvalidUsernameLength;
+            } else {
+                logicError = LogicErrorType.NoError;
+            }
+            return logicError;
+        }
+
+        /// <summary>
+        /// Helper method used to check the username length
+        /// </summary>
+        /// <param name="username">string username</param>
+        /// <returns>true/false</returns>
+        private bool CheckUsernameLength(string username) {
+            int usernameLength = username.Length;
+            return usernameLength < Profile.MinUsernameLength || usernameLength > Profile.MaxUsernameLength;
         }
     }
 }
--- AcademicReward/PopUps/AddMemberPopUp.xaml.cs
+++ AcademicReward/PopUps/AddMemberPopUp.xaml.cs
@@ -4,6 +4,7 @@ using AcademicReward.ModelClass;
 using AcademicReward.Resources;
 using CommunityToolkit.Maui.Views;
 using System.Collections.ObjectModel;
+using System.Text;
 
 namespace AcademicReward.PopUps;
 
@@ -28,7 +29,8 @@ public partial class AddMemberPopUp : Popup {
 		InitializeComponent();
 		this.group = group;
 		this.memberList = memberList;
-	}
+		SetErrorMessageBox(false, string.Empty);
+    }
 
 	/// <summary>
 	/// Method called when a user clicks the back button
@@ -43,38 +45,88 @@ public partial class AddMemberPopUp : Popup {
 	/// <param name="sender">object sender</param>
 	/// <param name="e">EventArgs e</param>
 	private void AddMemberButtonClicked(object sender, EventArgs e) {
+		LogicErrorType logicError;
 		// Grab the text from the entry box
 		String memberName = MemberNameEntry.Text;
-
-		// Error checking for empty string
-		if (memberName == null || memberName == "") {
-			// Set error label text
-			ErrorLabel.Text = "Please enter a member name";
-			return;
+		if(IsMemberNotInGroup(memberName)) {
+            // Add member logic
+            var addMemberLogic = new AddMemberLogic();
+            // Create an array with two elements, the current group and the member name
+            object[] obj = new object[] { memberName, group };
+			logicError = addMemberLogic.AddItemWithArgs(obj);
+			if(LogicErrorType.NoError == logicError) {
+                // Update the listview
+                memberList = GroupProfileRelationship.getProfilesInGroup(group);
+                Close(memberList);
+            }
+        } else {
+			logicError = LogicErrorType.MemberAlreadyInGroup;
 		}
-
-		// Add member logic
-		var addMemberLogic = new AddMemberLogic();
-
-		// Create an array with two elements, the current group and the member name
-		object[] obj = new object[] { memberName, group };
-
-		var error = addMemberLogic.AddItemWithArgs(obj);
-		if (error == LogicErrorType.GroupAlreadyHasAdmin) {
-            // Set error label text
-            ErrorLabel.Text = "An additional admin user cannot be added to a group";
-            return;
-        } else if (error != LogicErrorType.NoError) {
-            // Set error label text
-            ErrorLabel.Text = "Username not found";
-            return;
-        }
-
-        // Update the listview
-        memberList = GroupProfileRelationship.getProfilesInGroup(group);
-        
-		ErrorLabel.Text = "";
-        
-		Close(memberList);
+		//Something went wrong, show error message box
+		SetErrorMessageBox(true, SetErrorMessageBody(logicError));
 	}
+
+	/// <summary>
+	/// Helper method used to check if a given username is already in the group
+	/// </summary>
+	/// <param name="username">string username</param>
+	/// <returns>true/false</returns>
+	private bool IsMemberNotInGroup(string username) {
+		foreach(Profile member in memberList) {
+			if(member.Username.Equals(username)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+    /// <summary>
+    /// Helper method used to either show or hide the error message box
+    /// This is done since a popup cannot have another popup
+    /// </summary>
+    /// <param name="isVisible">bool isVisible</param>
+    /// <param name="errorMessage">string errorMessage</param>
+    private void SetErrorMessageBox(bool isVisible, string errorMessage) {
+        ErrorFrame.IsVisible = isVisible;
+        ErrorStackLayout.IsVisible = isVisible;
+        ErrorMessageHeader.IsVisible = isVisible;
+        ErrorMessageBody.IsVisible = isVisible;
+        ErrorMessageBody.Text = errorMessage;
+    }
+
+	/// <summary>
+	/// Helper method used to determine what error message to display.
+	/// </summary>
+	/// <param name="logicError">LogicErrorType logicError</param>
+	/// <returns>string errorMessage</returns>
+	private string SetErrorMessageBody(LogicErrorType logicError) {
+        StringBuilder errorMessageBuilder = new StringBuilder();
+		switch (logicError) {
+			case LogicErrorType.MemberAlreadyInGroup:
+				errorMessageBuilder.Append(DataConstants.SpaceDashSpace);
+				errorMessageBuilder.Append(DataConstants.AddMemberAlreadyInGroupMessage);
+				break;
+			case LogicErrorType.EmptyUsername:
+                errorMessageBuilder.Append(DataConstants.SpaceDashSpace);
+                errorMessageBuilder.Append(DataConstants.EmptyMemberNameMessage);
+                break;
+			case LogicErrorType.InvalidUsernameLength:
+                errorMessageBuilder.Append(DataConstants.SpaceDashSpace);
+                errorMessageBuilder.Append(DataConstants.InvalidMemberUsernameLengthMessage);
+                break;
+			case LogicErrorType.GroupAlreadyHasAdmin:
+                errorMessageBuilder.Append(DataConstants.SpaceDashSpace);
+                errorMessageBuilder.Append(DataConstants.GroupAlreadyHasAdminMessage);
+                break;
+			case LogicErrorType.GroupCreateError:
+                errorMessageBuilder.Append(DataConstants.SpaceDashSpace);
+                errorMessageBuilder.Append(DataConstants.AddMemberUserNameNotFoundMessage);
+                break;
+			default:
+                errorMessageBuilder.Append(DataConstants.SpaceDashSpace);
+                errorMessageBuilder.Append(DataConstants.AddMemberUnknownErrorMessage);
+                break;
+        }
+		return errorMessageBuilder.ToString();
+    }
 }
